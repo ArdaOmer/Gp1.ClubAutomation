4112c694f16e16f98ce8cbd98e3d6a2a96178b3c
ArdaOmer <ardaomer02@hotmail.com>
2025-10-13 23:17:12 +0300

    dotnet tools added

--- Gp1.ClubAutomation.Application/obj/Debug/net8.0/Gp1.ClubAutomation.Application.AssemblyInfo.cs
+++ Gp1.ClubAutomation.Application/obj/Debug/net8.0/Gp1.ClubAutomation.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gp1.ClubAutomation.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f6647546d7af9c29c1c50870b0918384c925b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2789c3111bab2dc780312270a9eabad1a184c1b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gp1.ClubAutomation.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gp1.ClubAutomation.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
